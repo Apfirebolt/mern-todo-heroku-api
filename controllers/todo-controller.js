76936a20df62a0e0c8059f942c848f5eaeabff42
Amit P <aspper20@gmail.com>
2022-01-19 01:32:50 +0530

    fixed minor typo bugs in controller

--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -1,11 +1,11 @@
 import asyncHandler from 'express-async-handler'
-import ToDo from '../models/ToDo.js'
+import ToDoModel from '../models/ToDo.js'
 
 // @desc    Get all todos related to logged in user
 // @route   GET /api/todos
 // @access  Private
 const getAllToDos = asyncHandler(async (req, res) => {
-  const todos = await ToDo.find({ createdBy: req.user._id })
+  const todos = await ToDoModel.find({ createdBy: req.user._id })
 
   if (todos) {
     res.status(200).json({
@@ -24,26 +24,26 @@ const getAllToDos = asyncHandler(async (req, res) => {
 const addToDo = asyncHandler(async (req, res) => {
   const { name } = req.body
 
-  const ToDoExists = await ToDo.findOne({ name, createdBy: req.user._id })
+  const toDoExists = await ToDoModel.findOne({ name, createdBy: req.user._id })
 
-  if (ToDoExists) {
+  if (toDoExists) {
     res.status(409)
-    throw new Error("ToDo already created by user")
+    throw new Error("To Do already created by user")
   }
 
-  const ToDo = await ToDo.create({
+  const toDo = await ToDoModel.create({
     name,
     createdBy: req.user._id
   })
 
-  if (ToDo) {
+  if (toDo) {
     res.status(201).json({
-      ToDo,
-      message: 'ToDo created'
+      toDo,
+      message: 'To Do created'
     })
   } else {
     res.status(401)
-    throw new Error("Invalid ToDo data")
+    throw new Error("Invalid To Do data")
   }
 })
 
@@ -52,18 +52,18 @@ const addToDo = asyncHandler(async (req, res) => {
 // @access  Private
 const updateToDo = asyncHandler(async (req, res) => {
 
-  const updatedToDo = await ToDo.findOneAndUpdate({ _id: req.params.id }, { name: req.body.name }, {
+  const updatedToDo = await ToDoModel.findOneAndUpdate({ _id: req.params.id }, { name: req.body.name }, {
     new: true
   })
 
   if (updatedToDo) {
     res.status(200).json({
-      ToDo: updatedToDo,
-      message: 'ToDo updated'
+      toDo: updatedToDo,
+      message: 'To Do updated'
     })
   } else {
     res.status(400)
-    throw new Error("Invalid ToDo data")
+    throw new Error("Invalid To Do data")
   }
 })
 
@@ -71,15 +71,15 @@ const updateToDo = asyncHandler(async (req, res) => {
 // @route   DELETE /api/todos/:id
 // @access  Private
 const deleteToDo = asyncHandler(async (req, res) => {
-  const ToDoDeleted = await ToDo.findOneAndDelete({ createdBy: req.user._id, _id: req.params.id })
+  const toDoDeleted = await ToDoModel.findOneAndDelete({ createdBy: req.user._id, _id: req.params.id })
 
-  if (ToDoDeleted) {
+  if (toDoDeleted) {
     res.status(200).json({
-      message: 'ToDo deleted successfully'
+      message: 'To Do deleted successfully'
     })
   } else {
     res.status(404)
-    throw new Error("ToDo not found")
+    throw new Error("To Do not found")
   }
 })
 
@@ -87,17 +87,17 @@ const deleteToDo = asyncHandler(async (req, res) => {
 // @route   GET /api/todos/:id
 // @access  Private
 const getToDo = asyncHandler(async (req, res) => {
-  const ToDo = await ToDo.findById({ _id: req.params.id })
+  const toDo = await ToDoModel.findById({ _id: req.params.id })
 
-  if (ToDo) {
-    if (toString(ToDo.createdBy._id) !== toString(req.user._id)) {
+  if (toDo) {
+    if (toString(toDo.createdBy._id) !== toString(req.user._id)) {
       res.status(403)
       throw new Error("Not authorized to access this content")
     }
-    res.status(200).json(ToDo)
+    res.status(200).json(toDo)
   } else {
     res.status(404)
-    throw new Error("ToDo not found")
+    throw new Error("To Do not found")
   }
 })
 
