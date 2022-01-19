107634d3e2b8ecc3daa67bff8940c36e4ac8e3f5
Amit P <aspper20@gmail.com>
2022-01-19 11:24:18 +0530

    added form components, browserrouter changed to hash

--- client/src/views/RegisterView.js
+++ client/src/views/RegisterView.js
@@ -1,11 +1,42 @@
-import React, { Fragment } from "react";
+import React from "react";
+import { Form, Button, Container } from "react-bootstrap";
 
 const RegisterPage = () => {
-  
   return (
-    <Fragment>
-      <p>Register Page</p>
-    </Fragment>
+    <Container>
+      <Form>
+        <Form.Group className="mb-3" controlId="username">
+          <Form.Label>Your Username</Form.Label>
+          <Form.Control type="text" placeholder="Enter username" />
+          <Form.Text className="text-muted">
+            You're required to have a unique username.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="firstName">
+          <Form.Label>Your First Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter first name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="lastName">
+          <Form.Label>Your Last Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter last name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
