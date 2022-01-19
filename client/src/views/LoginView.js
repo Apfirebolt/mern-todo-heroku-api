4448cad546c1f280aeaf54a137b82b466840b142
Amit P <aspper20@gmail.com>
2022-01-19 12:01:39 +0530

    added form validation through react form hooks

--- client/src/views/LoginView.js
+++ client/src/views/LoginView.js
@@ -1,21 +1,55 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Button, Container } from "react-bootstrap";
+import { login } from '../actions/authActions';
 
 const LoginPage = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const dispatch = useDispatch();
+
+  const onSubmit = async (values) => {
+    dispatch(login({ ...values }));
+  };
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            {...register("email", {
+              required: "Email field is required",
+              isEmail: "This must be valid email",
+            })}
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
+          </Form.Text>
+          <Form.Text className="text-danger d-block my-2">
+            {errors.email?.message}
           </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            {...register("password", {
+              required: "Password field is required",
+            })}
+          />
+          <Form.Text className="text-danger d-block my-2">
+            {errors.password?.message}
+          </Form.Text>
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
--- client/src/views/RegisterView.js
+++ client/src/views/RegisterView.js
@@ -1,36 +1,87 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { register as registerAction } from "../actions/authActions";
 import { Form, Button, Container } from "react-bootstrap";
 
 const RegisterPage = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const dispatch = useDispatch();
+
+  const onSubmit = async (values) => {
+    dispatch(registerAction({ ...values }));
+  };
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3" controlId="username">
           <Form.Label>Your Username</Form.Label>
-          <Form.Control type="text" placeholder="Enter username" />
+          <Form.Control
+            type="text"
+            placeholder="Enter username"
+            {...register("username", {
+              required: "Username field is required",
+            })}
+          />
           <Form.Text className="text-muted">
             You're required to have a unique username.
+          </Form.Text>
+          <Form.Text className="text-danger d-block my-2">
+            {errors.username?.message}
           </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="firstName">
           <Form.Label>Your First Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter first name" />
+          <Form.Control
+            type="text"
+            placeholder="Enter first name"
+            {...register("firstName")}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="lastName">
           <Form.Label>Your Last Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter last name" />
+          <Form.Control
+            type="text"
+            placeholder="Enter last name"
+            {...register("lastName")}
+          />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            {...register("email", {
+              required: "Email field is required",
+              isEmail: "This must be valid email",
+            })}
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
+          </Form.Text>
+          <Form.Text className="text-danger d-block my-2">
+            {errors.email?.message}
           </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            {...register("password", {
+              required: "Password field is required",
+            })}
+          />
+          <Form.Text className="text-danger d-block my-2">
+            {errors.password?.message}
+          </Form.Text>
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
