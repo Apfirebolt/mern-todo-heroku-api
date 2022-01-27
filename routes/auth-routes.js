11b8efc399b89ef19072e6dd94d78af4562bca40
Amit P <aspper20@gmail.com>
2022-01-28 03:09:02 +0530

    feat: added swagger docs

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -10,8 +10,55 @@ import {
 import { validateUser } from '../middleware/validate-user.js'
 import { protect } from '../middleware/auth-middleware.js'
 
-router.route('/register', ).post(validateUser, registerUser)
-router.post('/login', authUser)
+
+router.
+/**
+ * @swagger
+ * /api/auth/register:
+ *   post:
+ *     description: Register a new user
+ *     parameters:
+ *       - in: body
+ *         name: username
+ *         required: true
+ *       - in: body
+ *         name: email
+ *         required: true
+ *       - in: body
+ *         name: firstName
+ *         required: true
+ *       - in: body
+ *         name: lastName
+ *         required: true       
+ *     responses: 
+ *       200:
+ *         description: Success
+ *       405:
+ *         description: "Invalid Input"       
+ */
+  route('/register', ).post(validateUser, registerUser)
+router
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     description: Login with existing user Id
+ *     parameters:
+ *       - in: body
+ *         name: email
+ *         required: true
+ *       - in: body
+ *         name: password
+ *         required: true    
+ *     responses: 
+ *       200:
+ *         description: Success
+ *       405:
+ *         description: "Invalid Input"
+ *       401:
+ *         description: "Authentication Failed"       
+ */
+  .post('/login', authUser)
 router
   .route('/profile')
   .get(protect, getUserProfile)
--- server.js
+++ server.js
@@ -1,50 +1,89 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import morgan from 'morgan'
-import cors from 'cors';
-import connectDB from './config/db.js'
-import { notFound, errorHandler } from './middleware/error-handler.js'
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/error-handler.js";
 
 // import routes
-import authRoutes from './routes/auth-routes.js'
-import toDoRoutes from './routes/todo-routes.js'
+import authRoutes from "./routes/auth-routes.js";
+import toDoRoutes from "./routes/todo-routes.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'))
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.1", // YOU NEED THIS
+    info: {
+      title: "MERN Heroku Boilerplate",
+      description: "This is a simple ready to deploy MERN app on Heroku",
+      contact: {
+        name: "Apfirebolt",
+        email: "aspper20@gmail.com",
+      },
+      tags: [
+        {
+          name: "users",
+          description: "Users API",
+        },
+      ],
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: "http",
+            scheme: "bearer",
+            bearerFormat: "JWT",
+          },
+        },
+      },
+      security: [
+        {
+          bearerAuth: [],
+        },
+      ],
+      servers: ["http://localhost:5000/"],
+    },
+  },
+  apis: ["./routes/auth-routes.js", "./routes/todo-routes.js"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use('/api/auth', authRoutes)
-app.use('/api/todos', toDoRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/todos", toDoRoutes);
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Success' })
-})
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+app.get("/", (req, res) => {
+  res.json({ message: "Success" });
 });
 
-app.use(notFound)
-app.use(errorHandler)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
 
-const PORT = process.env.PORT || 5000
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
-  )
-)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
